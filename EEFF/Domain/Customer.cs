37b52e351c07f03f7990332519495132664974eb
pjvds <pj@born2code.net>
2011-02-18 14:59:09 +0100

    Added Events that will contains all the events and implemented customer creation.

--- EEFF/Domain/Customer.cs
+++ EEFF/Domain/Customer.cs
@@ -1,11 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Events;
+using Ncqrs.Domain;
 
 namespace Domain
 {
-    public class Class1
+    public class Customer : AggregateRootMappedByConvention
     {
+        private string _name;
+        private DateTime _birthDate;
+
+        protected Customer()
+        {
+            // DO NOT USE.
+        }
+
+        public Customer(string name, DateTime birthDate)
+        {
+            var e = new CustomerCreated{ Name = name, BirthDate = birthDate };
+            ApplyEvent(e);
+        }
+
+        private void OnCustomerCreated(CustomerCreated e)
+        {
+            _name = e.Name;
+            _birthDate = e.BirthDate;
+        }
     }
 }
---
+++ EEFF/Events/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Ncqrs.Eventing.Sourcing;
+
+namespace Events
+{
+    public class CustomerCreated : SourcedEvent
+    {
+        public Guid CustomerId { get { return EventSourceId;  } }
+
+        public String Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
