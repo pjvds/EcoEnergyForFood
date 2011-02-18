bb616d2261b017f184d154a14c33ca46a6c357fc
pjvds <pj@born2code.net>
2011-02-18 15:04:08 +0100

    Added Commands project and first command CreateCustomer.

---
+++ EEFF/Commands/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Ncqrs.Commanding;
+
+namespace Commands
+{
+    public class CreateCustomer : CommandBase
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
